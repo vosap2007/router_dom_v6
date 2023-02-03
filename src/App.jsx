1358e64e58be88bbb748843cf4d59d86cab11468
vosap2007 <65125720+vosap2007@users.noreply.github.com>
2023-02-03 13:57:03 +0200

    adds "react-router-dom": "^6.8.0",

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Link,
+  Outlet,
+  RouterProvider,
+} from 'react-router-dom';
+import Contact from './Contact';
+import Data from './Data';
+import Home from './Home';
 import './index.css';
 
-function App() {
+function App(props) {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path="/data" element={<Data />} />
+        <Route path="/contact" element={<Contact />} />
+      </Route>
+    )
+  );
+
   return (
     <div className="App">
-      <h1>HELLO REACT!!!</h1>
+      <RouterProvider router={router} />
     </div>
   );
 }
 
 export default App;
+
+const Root = () => {
+  return (
+    <>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/data">Data</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+};
